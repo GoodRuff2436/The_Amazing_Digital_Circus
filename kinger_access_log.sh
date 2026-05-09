b1dc6903f5fbeb4920711f77fb82133e9602c84a
good_ruff2436 <itschool.ikeyama@gmail.com>
2026-05-09 14:18:43 +0900

    Create kinger_access_log.sh

---
+++ kinger_access_log.sh
@@ -0,0 +1,174 @@
+Ok, so, because I know a lot of people were interested in it, here’s the complete coding of what Kinger was doing on the computer. I indicate when the scene cuts away. Also, as you know, images of Caine and Bubble started appearing on the screen and covered up parts of the coding that I simply couldn’t see no matter what. For the parts that were covered up, I put in (BCR), which stands for “blocked, can’t read”. Make from this what you will.
+
+ 
+
+ 
+
+0 System: KingSolution 2.0. / Digital Circus Mainframe
+0 Date: 1996-10-30
+0 Host: circus digital
+“Initializing telnet connection…”
+Login: “kinger”
+Pass: queenie123
+kinger@circus: “$ whoami
+kinger - adminstra
+
+ 
+
+(Scene cuts away)
+
+ 
+
+kinger@circus: “$ grep Ai-Location
+root 1337 /usr/ai/agent/caine
+root 1338 /usr/ai/agent/experimental
+root 1339 /usr/ai/module/consciousnessresearch
+root 1340 /usr/ai/module/brainscans
+kinger@circus “$ /secured/
+
+total 8492
+
+drwxr-xp-x 3 root wheel 512 Oct 15 1996 .
+drwxr-xp-x 45 root wheel 1024 Oct 15 1996 ..
+-rwxr-xp-x 1 root wheel 892344 Oct 15 1996 caine-core.lisp
+-rwxr-xp-x 1 root wheel 234512 Oct 15 1996 paraphernalin-engine.dat
+-rwxr-xp-x 1 root wheel 234512 Oct 15 1999 [Scratch].dat
+-rwxr-xp-x 1 root wheel 234512 Oct 15 2008 [Ragatha].dat
+
+…
+
+…
+
+-rwxr-xp-x 1 root wheel 45632 Oct 15 1996 wacky-watch.c
+-rwxr-xp-x 1 root wheel 78234 Oct 15 1996 bubble-chef.lisp
+kinger@circus “$ stop caine process
+ 
+
+WARNING: $ %WHOOPS WRONG APPROACH THERE”%”
+
+kinger@circus “$ /usr/bin/gdb /usr/local/bin/clisp 1337
+
+gdb: ptrace: Operation not permitted
+
+ERROR: Protected by 57x immersive AI defense system
+
+kinger@circus “$ chmod 000 /secured/caine-core.lisp
+
+chmod: /secured/caine-core.lisp: Permission denied
+
+WARNING: Unfinished work detected. Access restricted.
+
+kinger@circus “$ rm /secured/paraphernalia-engine.dat
+
+rm: /secured/paraphernalia-engine.dat: Permission denied
+
+ERROR▓ Can/not inject term |-nt. Torment must be 100% ac<iden+al +%Y
+
+kinger@circus “$
+
+ 
+
+(Scene cuts away)
+
+ 
+
+mount: only root can do that
+
+NOTE: Hundreds of all-seeing eyes are watching!
+
+$: GASP! A CRITICAL MALFUNCTION in my SPECTACULAR systems!
+$: Unauthorized isolation attempt triggered EMERGENCY PROTOCALS!
+$: DESTRUCTIIVE WACKYTIME initiated! Lockout load sequence INITIATE!
+
+ 
+
+WACKYTIME_LOCKOUT: ░░▓▓▓▓▓▓▓▓░ 20% loaded
+
+kinger@circus “$ systemctl stop WACKYTIME_LOCKOUT
+
+$: On what GROUNDS are your Authority?
+
+kinger@circus “$ ./GreenGROUNDS --daemon --target=torment_injection a
+
+$: “SECURITY ALERT: Multiple exploit attempts logged”
+
+$: WHOA when did you make THAT?
+
+$: I must hand it to y*u G̣▓αη▓, y0ur mind was a(ways resourceâul.
+
+kinger@circus “$ –u kinger //securitysweep_stealth
+
+$: Abort fallback procedure [Y/N]
+
+kinger@circus “$ Y
+
+$: Aborting fallback requires ADMINISTRATOR confirmation?
+
+$: Please enter code:
+
+kinger@circus “$ admin1234
+
+$: INCORRECT! That’s not even CLOSE to wacky enough!
+
+$: Retry with different code? [Y/N]
+
+kinger@circus “$ Y
+
+WACKYTIME_LOCKOUT: ░░░░▓▓▓▓▓▓░ 40% loaded
+
+$: Enter WACKY code now:
+
+kinger@circus “S PARAPHERMALIA
+
+WACKYTIME: 35 seconds remaining
+
+$: That IS a wacky word! But WRONG code!
+$: System selecting SAFEST option for stability…
+$: Cancel automatic selection? [Y/N]
+
+kinger@circus: “$ Y
+
+WACKYTIME_LOCKOUT: ░░░░░▓▓▓▓▓░ 50% loaded
+
+$: Which backup do you want? [A/B/C]
+
+kinger@circus: “$ C
+
+$: NONE selected! Interpreted as: DELETION(BCR)NAME!
+$: Confirm deletion of current unstable(BCR)
+
+kinger@circus: “$ N
+
+$: Negative response? Inverting to DEACTIVE per emergency protocols?
+
+$: JUST DELETE THIS M***********, HAHAHAHAaaaaaaaaaaaaaaaaa
+
+kinger@circus: “$ N
+
+WACKYTIME_LOCKOUT: ░░░░░░░░▓▓░ 80% loaded
+
+$: IGNORED! Proceeding with ORIGINAL de(BCR)
+$: Actually you’re CONFUSED let me HELP (BCR)t module to EXIST? [Y/N]
+
+kinger@circus: “$ Y
+
+$: Override instruction error falling back to default method
+
+kinger@circus: “$ ./Switcheroo_reality --daemon --target=torment_injection a
+
+$: Fusion --b(BCR) program1 program2 --daemon
+$: Are you sure you want to delete caine? [Y/N]
+
+WACKYTIME_LOCKOUT: ░░░░░░░░░▓░ 90% loaded
+
+kinger@circus: “$ Y
+kinger@circus: “$ ./|ABORT Rollback --depth=1 --force --protocol G WVJI
+kinger@circus: “$ ^C
+
+DESTRUCTIVE WACKYTIME Lockout Load Sequence: COMPLETE
+
+ 
+
+ 
+
+That’s everything. From the looks of it, when Kinger was coding, he was communicating with someone named $, which I’m assuming stands for “system”. Interestingly, even though Kinger accidentally hit the “delete” key, $ was trying to get Kinger to delete Caine. Towards the end there, it also mentioned something about “fusion” and “program1” and “program2”. I’m assuming program1 is Caine and program2 is Abel, and the whole fusion thing involved Caine consuming Abel (the red and blue dots). But it also mentions targeting something called “daemon” a couple times. Maybe Daemon was the name of the blue dot rather than Abel. But I’m just brainstorming at this point.
